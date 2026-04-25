8f9868ed8d84be31eb7616ed590f0521b115d21d
P. Shanmukha Sai <89543859+Shanmukhsvstp@users.noreply.github.com>
2026-04-26 00:53:29 +0530

    backend fundamentals setup

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,13 @@
+import uvicorn
 from fastapi import FastAPI
 from app.routes import user
+from app.routes import auth
 
 app = FastAPI()
 
-app.include_router(user.router)
+app.include_router(auth.router, prefix="/api")
+app.include_router(user.router, prefix="/api")
+
+
+if __name__ == "__main__":
+    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
