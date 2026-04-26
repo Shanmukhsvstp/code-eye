df5711bfdc21e993e4ad9b2c65a5859edf3b0374
P. Shanmukha Sai <89543859+Shanmukhsvstp@users.noreply.github.com>
2026-04-27 00:49:39 +0530

    db

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,11 +2,18 @@ import uvicorn
 from fastapi import FastAPI
 from app.routes import user
 from app.routes import auth
+from fastapi import APIRouter
 
 app = FastAPI()
 
-app.include_router(auth.router, prefix="/api")
-app.include_router(user.router, prefix="/api")
+api_router = APIRouter(prefix="/api")
+
+api_router.include_router(auth.router)
+api_router.include_router(user.router)
+
+
+# app.include_router(auth.router, prefix="/api")
+# app.include_router(user.router, prefix="/api")
 
 
 if __name__ == "__main__":
