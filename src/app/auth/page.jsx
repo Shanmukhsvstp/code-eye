5bc809606a13c3db493947c171290f8c8cbbe82f
Pitani Shanmukha Sai <89543859+Shanmukhsvstp@users.noreply.github.com>
2026-04-27 13:51:04 +0000

    feat: update Auth component styles and structure for improved responsiveness

--- src/app/auth/page.jsx
+++ src/app/auth/page.jsx
@@ -3,7 +3,9 @@ import styles from './Auth.module.css';
 export default function Auth() {
     return (
         <div className={styles.main}>
-            <div className={styles.card}></div>
+            <div className={styles.card}>
+                <p className={styles.title}>Get Started</p>
+            </div>
         </div>
     )
 }
